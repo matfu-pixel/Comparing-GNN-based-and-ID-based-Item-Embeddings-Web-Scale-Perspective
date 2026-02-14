Fix normalization & Log hitrate

gnn_utils/losses.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-from gnn_utils.metrics import inbatch_mrr, inbatch_recall_at_k
+from gnn_utils.metrics import inbatch_hitrate_at_k, inbatch_mrr
 from gnn_utils.utils import make_pairs
 
 
@@ -17,7 +17,9 @@ class TwhinLoss(nn.Module):
         num_negatives = batch_size * batch_size - batch_size
         num_positives = batch_size
         neg_weight = float(num_positives) / num_negatives
-        dot_products = torch.matmul(source_embeddings, target_embeddings.t())
+        dot_products = torch.matmul(
+            torch.nn.functional.normalize(source_embeddings), torch.nn.functional.normalize(target_embeddings).t()
+        )
 
         logits = torch.cat(
             [dot_products.diag(), dot_products.flatten()[1:].view(batch_size - 1, batch_size + 1)[:, :-1].flatten()]
@@ -56,11 +58,11 @@ class InBatchSampledSoftmax(nn.Module):
     def temperature(self):
         return torch.clip(torch.exp(self.tau), min=self.MIN_TEMPERATURE, max=self.MAX_TEMPERATURE)
 
-    def forward(self, queries, candidates, recall_at=None):
+    def forward(self, queries, candidates, hitrate_at=None):
         logits = self.temperature * queries @ candidates.T
-        if recall_at is None:
+        if hitrate_at is None:
             return -torch.log_softmax(logits, dim=1).diag().mean()
-        return -torch.log_softmax(logits, dim=1).diag().mean(), inbatch_recall_at_k(queries, candidates, recall_at)
+        return -torch.log_softmax(logits, dim=1).diag().mean(), inbatch_hitrate_at_k(queries, candidates, hitrate_at)
 
 
 class CalibratedPairwiseLogistic(nn.Module):

gnn_utils/metrics.py
@@ -1,13 +1,15 @@
+from collections import defaultdict
+
 import torch
 
 
 @torch.no_grad()
-def inbatch_mrr(dot_products: torch.Tensor) -> torch.Tensor:
+def inbatch_mrr(dot_products: torch.Tensor) -> list[float]:
     """
     dot_products: [B, B] where dot_products[i, j] is score of source i vs target j,
     and the positive for source i is at j == i.
 
-    Returns: scalar MRR for this batch (torch.Tensor).
+    Returns: list of MRR scores for this batch.
     """
     B = dot_products.size(0)
     sorted_idx = dot_products.argsort(dim=1, descending=True)  # [B, B]
@@ -17,29 +19,29 @@ def inbatch_mrr(dot_products: torch.Tensor) -> torch.Tensor:
     ranks = positions.to(torch.float32) + 1.0  # [B]
 
     rr = 1.0 / ranks
-    mrr = float(rr.mean())
-    return mrr
+    return rr.cpu().float().tolist()
 
 
 @torch.no_grad()
-def inbatch_recall_at_k(queries: torch.Tensor, candidates: torch.Tensor, ks=(10, 100, 1000)) -> dict:
+def inbatch_hitrate_at_k(queries: torch.Tensor, candidates: torch.Tensor, ks=(10, 100, 1000)) -> dict[int, list[float]]:
     """
     queries:    [B, D]
     candidates: [B, D]
     Assumes the positive for query i is candidate i.
 
-    Returns: dict {k: recall_at_k} where recall_at_k is a scalar tensor.
+    Returns: dict {k: hitrate_at_k} where hitrate_at_k is a boolean tensor of shape [B].
     """
     scores = queries @ candidates.T  # [B, B]
     B = scores.size(0)
-    max_k = min(max(ks), B)  # cannot be larger than batch size
+
+    max_k = max(ks)
+    assert max_k <= B, "max_k cannot be larger than batch size"
 
     topk_idx = scores.topk(k=max_k, dim=1).indices  # [B, max_k]
     target_indices = torch.arange(B, device=scores.device).unsqueeze(1)  # [B, 1]
 
-    recalls = {}
+    hitrates = defaultdict(list)
     for k in ks:
-        k_eff = min(k, max_k)  # clamp if k > B
-        hits = (topk_idx[:, :k_eff] == target_indices).any(dim=1).float()  # [B]
-        recalls[k] = float(hits.mean())
-    return recalls
+        hits = (topk_idx[:, :k] == target_indices).any(dim=1).float()  # [B]
+        hitrates[k].extend(hits.cpu().tolist())
+    return hitrates

gnn_utils/models.py
@@ -19,8 +19,8 @@ class TwhinModel(nn.Module):
         user_embeddings = self.user_embeddings(inputs["users"])
         relation_embeddings = self.relation_embeddings(inputs["relations"])
         return self.criterion(
-            torch.nn.functional.normalize(user_embeddings + relation_embeddings),
-            torch.nn.functional.normalize(item_embeddings),
+            user_embeddings + relation_embeddings,
+            item_embeddings,
             calculate_mrr=calculate_mrr,
         )
 
@@ -78,12 +78,15 @@ class ModelBackbone(nn.Module):
             activation="gelu",
             batch_first=True,
         )
-        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_transformer_layers)
+        self.transformer_encoder = nn.TransformerEncoder(
+            encoder_layer, num_transformer_layers, enable_nested_tensor=False
+        )
+        self.projection_layer = nn.Linear(embedding_dim, hidden_dim)
         self.user_encoder = PickFirst()
         self.item_encoder = nn.Sequential(
             nn.Linear(embedding_dim, embedding_dim * 4),
             *[ResNet(embedding_dim=embedding_dim * 4) for _ in range(num_transformer_layers)],
-            nn.Linear(embedding_dim * 4, embedding_dim),
+            nn.Linear(embedding_dim * 4, hidden_dim),
         )
 
     def forward(self, inputs):
@@ -105,7 +108,7 @@ class ModelBackbone(nn.Module):
         cls_embedding = self.cls_embedding(
             torch.zeros(batch_size, 1, device=inputs["product_id"].device, dtype=torch.long)
         )
-        input_embeddings_with_cls = torch.cat([cls_embedding, input_embeddings], dim=1)
+        input_embeddings_with_cls = self.projection_layer(torch.cat([cls_embedding, input_embeddings], dim=1))
 
         padding_mask = torch.cat(
             [
@@ -115,7 +118,7 @@ class ModelBackbone(nn.Module):
             dim=1,
         )
         user_embeddings = self.user_encoder(
-            self.transformer_encoder(src=input_embeddings_with_cls, src_key_padding_mask=padding_mask)
+            self.transformer_encoder(src=self.dropout(input_embeddings_with_cls), src_key_padding_mask=padding_mask)
         )
         offsets = torch.cumsum(inputs["candidate_names"]["lengths"], dim=0) - inputs["candidate_names"]["lengths"]
         candidates_embeddings = self.item_encoder(
@@ -134,11 +137,11 @@ class PretrainModel(nn.Module):
         self.backbone = backbone
         self.criterion = InBatchSampledSoftmax()
 
-    def forward(self, inputs, recall_at=None):
+    def forward(self, inputs, hitrate_at=None):
         backbone_output = self.backbone(inputs)
         user_embeddings = backbone_output["user_embeddings"]
         candidates_embeddings = backbone_output["candidates_embeddings"]
-        return self.criterion(user_embeddings, candidates_embeddings.squeeze(1), recall_at=recall_at)
+        return self.criterion(user_embeddings, candidates_embeddings.squeeze(1), hitrate_at=hitrate_at)
 
 
 class FinetuneModel(nn.Module):